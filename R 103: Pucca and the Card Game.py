354b8d01eff2b9dd5749009a74bdc003c4075e75
Sonal Shabir <103493764+sonal1502@users.noreply.github.com>
2023-03-06 14:32:07 +0530

    Create R 103: Pucca and the Card Game.py

---
+++ R 103: Pucca and the Card Game.py
@@ -0,0 +1,16 @@
+n=int(input())
+a=list(map(int,input().split()))
+pucca=garu=f=i=0;j=n-1
+while i<=j:
+    if f==0:
+        if a[i]>=a[j]: pucca+=a[i]; i+=1
+        else: pucca +=a[j];j-=1
+        f=1
+    else: 
+        if a[i]>=a[j]: garu+=a[i];i+=1
+        else: garu+=a[j];j-=1
+        f=0
+print('Pucca' if pucca>=garu else 'Garu')
+
+
+
