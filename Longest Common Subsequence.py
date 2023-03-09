c68426229d28ec9ca8d53c100c53a700350403ac
Sonal Shabir <103493764+sonal1502@users.noreply.github.com>
2023-03-09 14:03:43 +0530

    Create Longest Common Subsequence.py

---
+++ Longest Common Subsequence.py
@@ -0,0 +1,12 @@
+a=input()
+b=input()
+m1=0;n=len(a);m=len(b)
+dp=[[0]*(m+1) for i in range (n+1)]
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        if a[i-1]==b[j-1]:dp[i][j]=dp[i-1][j-1]+1
+        else: dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        m1=max(m1,dp[i][j])
+        print(dp[i][j],end=' ')
+    print()
+print(m1)
